e25b58a84cb91a36543b5d95ff1c87b6bf6a9ac4
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-03-05 21:45:24 +0530

    fixed dependancy

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import discord
 import os
 
-client= discord.Client()
+client = discord.Client()
+
 
 @client.event
-
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
-
 async def on_message(message):
-  if message.author== client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('hello' or 'hi'):
-    await message.channel.send("Hello ! I'm Innovo Bot. If you have any questions about our server , I'd be glad to help you")  
-  
-  client.run(os.getenv('TOKEN'))
+    if message.content.startswith('hello' or 'hi'):
+        await message.channel.send(
+            "Hello ! I'm Innovo Bot. If you have any questions about our server , I'd be glad to help you")
+
+    client.run(os.getenv('TOKEN'))
